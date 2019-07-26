e49009b5ef24d860404cd3e090459159f32f9032
robin <838579661@qq.com>
2019-07-26 16:33:01 +0800

    将git_test_project添加到本地仓库

---
+++ git_test_project/src/com/itheima/git/First.java
@@ -0,0 +1,9 @@
+package com.itheima.git;
+
+public class First {
+    public static void main(String[] args) {
+        for (int i = 0; i <10 ; i++) {
+            System.out.println("hello,git!!!!");
+        }
+    }
+}
